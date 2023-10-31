807451c5c15f141a5596d84e197a641ca576aacb
i_rk_jain <svsravikiran.9@gmail.com>
2023-10-31 19:29:48 +0530

    Timer

--- Assets/Scripts/ScoreAndTimer/ScoreAndTimer.cs
+++ Assets/Scripts/ScoreAndTimer/ScoreAndTimer.cs
@@ -2,21 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreAndTimer : MonoBehaviour {
 
     [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private TextMeshProUGUI timerText;
+    [SerializeField] private Slider timerbar;
 
     private int score = 0;
+    private int scorePerPolice = 10;
+
+    private float tStart;
+    private float totalTime = 200;
 
     private void Start() {
         PoliceService.Instance.OnPoliceCarDead += OnPoliceCarDead;
 
         AddScoreAndUpdate(0);
+
+        tStart = Time.time;
+    }
+
+    private void Update() {
+        float timePassed = Time.time - tStart;
+        float timeLeft = totalTime - timePassed;
+
+        timerText.text = ((int)timeLeft).ToString();
+        timerbar.value = timePassed / totalTime;
     }
 
     private void OnPoliceCarDead() {
-        AddScoreAndUpdate(10);
+        AddScoreAndUpdate(scorePerPolice);
     }
 
     private void AddScoreAndUpdate(int n) {
